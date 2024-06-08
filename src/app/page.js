a299d39c413ff8f0c770cdda65622ff2ec3ca0ba
tnakagawa <nakagat@gmail.com>
2024-06-08 11:44:43 +0900

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,21 @@
+import { sql } from "@vercel/postgres";
 
-export default function Home() {
+export default async function Home() {
+  const { rows } = await sql`SELECT LOCALTIME`;
+
   return (
     <main className="flex flex-col p-6">
-      <div className="text-xl">Hello</div>
-      <div>{process.env.TEST_ENV}</div>
+      <div className="flex place-content-center">
+        <div className="text-xl">Hello</div>
+      </div>
+      <div className="flex place-content-center">
+        <div>{process.env.TEST_ENV}</div>
+      </div>
+      <div className="flex place-content-center">
+        <div>
+          {rows[0].localtime}
+        </div>
+      </div>
     </main>
   );
 }
